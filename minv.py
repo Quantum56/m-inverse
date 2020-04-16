411ae0a631d1ac0c9cf21b896aca64e7b6bcda0e
Quantum56 <bartleyzack@gmail.com>
2020-04-16 17:08:27 -0400

    final commit

--- minv.py
+++ minv.py
@@ -28,7 +28,7 @@ def main():
         r3 = str(input()).split()
         r1 = [float(i) for i in r1]
         r2 = [float(i) for i in r2]
-        r3 = [float(i) for i in r2]
+        r3 = [float(i) for i in r3]
         x = np.array([r1, r2, r3], dtype='float')
     elif(n == 4):
         print("Enter row 1 separated by spaces (e.g. 1 2 3 4):")
@@ -44,10 +44,14 @@ def main():
         r3 = [float(i) for i in r3]
         r4 = [float(i) for i in r4]
         x = np.array([r1, r2, r3, r4], dtype='float')
-    inv = np.linalg.inv(x)
-    print(inv)
+    inva = getMatrixInverse(x.tolist())
+    print('\n')
+    for ab in range(len(inva)):
+        print(inva[ab])
+    inv = np.array(list(inva))
     print('Attempt fractions? (y/n)')
     yn = str(input())
+    print('\n')
     if('y' in yn or 'yes' in yn):
         i = 0
         j = 0
@@ -70,6 +74,45 @@ def doublearr(na):
             li[ia].append(na)
         ia+=1
     return li
+
+
+def transposeMatrix(m):
+    return map(list,zip(*m))
+
+def getMatrixMinor(m,i,j):
+    return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
+
+def getMatrixDeternminant(m):
+    #base case for 2x2 matrix
+    if len(m) == 2:
+        return m[0][0]*m[1][1]-m[0][1]*m[1][0]
+
+    determinant = 0
+    for c in range(len(m)):
+        determinant += ((-1)**c)*m[0][c]*getMatrixDeternminant(getMatrixMinor(m,0,c))
+    return determinant
+
+def getMatrixInverse(m):
+    determinant = getMatrixDeternminant(m)
+    #special case for 2x2 matrix:
+    if len(m) == 2:
+        return [[m[1][1]/determinant, -1*m[0][1]/determinant],
+                [-1*m[1][0]/determinant, m[0][0]/determinant]]
+
+    #find matrix of cofactors
+    cofactors = []
+    for r in range(len(m)):
+        cofactorRow = []
+        for c in range(len(m)):
+            minor = getMatrixMinor(m,r,c)
+            cofactorRow.append(((-1)**(r+c)) * getMatrixDeternminant(minor))
+        cofactors.append(cofactorRow)
+    cofactors = list(transposeMatrix(cofactors))
+    for r in range(len(cofactors)):
+        for c in range(len(cofactors)):
+            cofactors[r][c] = cofactors[r][c]/determinant
+    return cofactors
+
 
 if(__name__=="__main__"):
     main()
