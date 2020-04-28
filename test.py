bccd2c6d2744c7dd453a3366aa5c0e9d6560fc84
Quantum56 <bartleyzack@gmail.com>
2020-04-28 01:19:23 -0400

    fixed tests

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-import webbrowser
-import os
+import os.path
 from ctypes import *
 from random import random
 from math import sqrt
@@ -7,9 +6,9 @@ import multiprocessing
 from joblib import Parallel, delayed
 import time
 
-nums = []
-
 def main():
+    nums = []
+    print("Pi Calculator using Monte-Carlo Method (C) Zack Bartley")
     i = int(input("Input number of iterations: "))
     j = int(input("Input number of times iterated: "))
     jobs = []
@@ -17,16 +16,33 @@ def main():
         p = multiprocessing.Process(target=monteCarlo, args=(i,))
         jobs.append(p)
         p.start()
-    for job in jobs:
-        job.join()
-    fa = open("numslist.txt", "r")
-    for x in fa:
+    p.join()
+    fa = open("numslist.txt", "a+")
+    ia = 1
+    while os.path.isfile(str(ia) + ".txt"):
+        f1 = open(str(ia) + ".txt")
+        y1 = f1.readline()
+        fa.write(y1)
+        ia += 1
+        f1.close()
+    fa.close()
+    fb = open("numslist.txt", "r")
+    for x in fb:
         nums.append(float(x))
     print(average(nums))
-    os.remove("numslist.txt")
-    
+    fb.close()
+    deleteFunc()
+
 def average(n):
     return sum(n) / len(n)
+
+def deleteFunc():
+    dir_name = os.getcwd()
+    test = os.listdir(dir_name)
+
+    for item in test:
+        if item.endswith(".txt"):
+            os.remove(os.path.join(dir_name, item))
 
 def monteCarlo(ia):
     inside = 0
@@ -42,8 +58,14 @@ def monteCarlo(ia):
             outside += 1
 
     ratio = (inside / (outside + inside))
-    print(4*ratio)
-    f = open("numslist.txt", "a")
+    boll = False
+    a = 1
+    while not boll:
+        if not os.path.isfile(str(a) + ".txt"):
+            f = open((str(a) + ".txt"), "w")
+            boll = True
+        else:
+            a += 1
     f.write("{}\n".format(str(4*ratio)))
     f.close()
 
